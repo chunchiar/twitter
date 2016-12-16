86d228cf46209257f6ccdca41fb59d6f5243d272
chunchiar <chunchiar_7@hotmail.com>
2016-12-16 13:11:52 +0800

    complete follower relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root 'pages#index'
+  resources :users do
+      member do
+        get :following, :followers
+      end
+    end
+  resources :relationships, only: [:create, :destroy]
   resources :posts
+
+  root 'pages#index'
   get '/home' => 'pages#home'
-
   get '/user/:id' => 'pages#profile'
-
   get '/explore' => 'pages#explore'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
